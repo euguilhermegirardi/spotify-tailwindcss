c87fddcd2af7a31caec314c0b01b30e359d6bdbe
Guilherme Girardi <guilherme@ardanis.com>
2024-01-19 15:59:54 -0300

    set min-width for nav and library

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="text-white h-screen bg-black-0 font-roboto md:flex md:flex-col md:p-2">
       <div className="flex gap-2 overflow-hidden">
-        <div className="hidden md:flex md:flex-2 md:flex-col md:gap-2">
+        <div className="hidden md:flex md:min-w-[371.71px] md:flex-2 md:flex-col md:gap-2">
           <Nav />
           <Library />
         </div>
