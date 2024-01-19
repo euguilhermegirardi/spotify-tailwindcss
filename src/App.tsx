e616322d2a9bd0fbd199150e735702f84f1a90dd
Guilherme Girardi <guilherme@ardanis.com>
2024-01-19 11:53:21 -0300

    tidy-up

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Player from './components/player'
 export default function App() {
   return (
     <div className="text-white h-screen bg-black-0 font-roboto md:flex md:flex-col md:p-2">
-      <div className="flex overflow-hidden">
+      <div className="flex gap-2 overflow-hidden">
         <div className="hidden md:flex md:flex-2 md:flex-col md:gap-2">
           <Nav />
           <Library />
